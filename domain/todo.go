809a90a54d7df91a26bc82a744a4a62593b5ec4b
Takahiro Oshima <tarotora51@gmail.com>
2018-11-18 20:56:43 +0900

    Todoモデルの修正

--- domain/todo.go
+++ domain/todo.go
@@ -1,8 +1,8 @@
-package domain
+package todo
 
 type Todo struct {
-	Id      string `json:id`
-	User    string `json:user`
-	Subject string `json:subject`
-	Status  string `json:status`
+	Id      string `json:"id"`
+	User    string `json:"user"`
+	Subject string `json:"subject"`
+	Status  string `json:"status"`
 }
